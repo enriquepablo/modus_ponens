1a163c81a3e15c591c51fb7a3f45de32cb2ceee2
enriquepablo <enrique@cazalla.net>
2020-02-16 14:24:33 +0100

    more complex grammar

--- src/factset.rs
+++ src/factset.rs
@@ -87,4 +87,45 @@ mod tests {
         let resp5 = kb.ask("<X0> ISA animal.");
         assert_eq!(resp5, false);
     }
+    #[test]
+    fn test_2() {
+        let mut kb = Knowledge::new();
+        kb = kb.tell("\
+            susan ISA person.\
+            john ISA person.\
+            person IS animal.\
+            (say: susan, what: (want: susan, what: (love: john, who: susan))) ISA fact.\
+            (want: john, what: (love: john, who: susan)) ISA fact.\
+            (love: susan, who: john) ISA fact.");
+        let resp1 = kb.ask("susan ISA person.");
+        assert_eq!(resp1, true);
+        let resp2 = kb.ask("pepe ISA person.");
+        assert_eq!(resp2, false);
+        let resp3 = kb.ask("john ISA person.");
+        assert_eq!(resp3, true);
+        let resp4 = kb.ask("<X0> ISA person.");
+        assert_eq!(resp4, true);
+        let resp5 = kb.ask("<X0> ISA animal.");
+        assert_eq!(resp5, false);
+        let resp5 = kb.ask("<X0> IS animal.");
+        assert_eq!(resp5, true);
+        let resp6 = kb.ask("(say: susan, what: (want: susan, what: <X0>)) ISA fact.");
+        assert_eq!(resp6, true);
+        let resp6 = kb.ask("(say: john, what: (want: susan, what: <X0>)) ISA fact.");
+        assert_eq!(resp6, false);
+        let resp7 = kb.ask("(want: <X0>, what: (love: <X0>, who: <X1>)) ISA fact.");
+        assert_eq!(resp7, true);
+        let resp8 = kb.ask("(want: <X0>, what: (love: <X0>, who: <X0>)) ISA fact.");
+        assert_eq!(resp8, false);
+        let resp9 = kb.ask("(say: susan, what: <X0>) ISA fact.");
+        assert_eq!(resp9, true);
+        let resp10 = kb.ask("(want: susan, what: <X0>) ISA fact.");
+        assert_eq!(resp10, false);
+        let resp11 = kb.ask("(want: john, what: <X0>) ISA fact.");
+        assert_eq!(resp11, true);
+        let resp11 = kb.ask("(love: john, who: susan) ISA fact.");
+        assert_eq!(resp11, false);
+        let resp11 = kb.ask("(love: susan, who: john) ISA fact.");
+        assert_eq!(resp11, true);
+    }
 }
