30d536f4101c86f3023e3c9fc1c57b73c7a60d17
enriquepablo <enrique@cazalla.net>
2020-05-16 09:35:55 +0200

    benchmark clips and mopo

--- examples/isa-bench/src/main.rs
+++ examples/isa-bench/src/main.rs
@@ -10,6 +10,8 @@ extern crate pest_derive;
 use std::mem;
 use std::time::SystemTime;
 use structopt::StructOpt;
+use std::{thread, time};
+
 
 use crate::modus_ponens::kbase::KBGen;
 use crate::modus_ponens::kbase::KBase;
@@ -22,11 +24,19 @@ mod kb;
 struct Opt {
     /// number of facts
     #[structopt(short, long)]
-    facts: i32,
+    facts: usize,
 
     /// number of rules
     #[structopt(short, long)]
-    rules: i32,
+    rules: usize,
+
+    /// number of samples per batch
+    #[structopt(short, long)]
+    samples: usize,
+
+    /// take batch of samples every so many rules
+    #[structopt(short, long)]
+    report: usize,
 }
 
 fn main() {
@@ -36,40 +46,86 @@ fn main() {
 
     let sets = ["thing", "animal", "mammal", "primate", "human"];
     let nsets = 5;
-    let t0 = SystemTime::now();
+        
+    let num_r = 2;
+    let num_f = opt.facts + 4;
 
+    let o_one_sec = time::Duration::from_millis(100);
+    let t0 = SystemTime::now();
+    let mut start = 0;
+    
     for r in 0..opt.rules {
-        let f1 = format!("<X0> ISA{r} <X1>; <X1> IS{r} <X2> -> <X0> ISA{r} <X2>.", r = r);
+        start += 1;
+        let f1 = format!("<X0> ISA{start} <X1>; <X1> IS{start} <X2> -> <X0> ISA{start} <X2>.", start = start);
         kb.tell( unsafe { mem::transmute( f1.as_str() ) });
-        let f2 = format!("<X0> IS{r} <X1>; <X1> IS{r} <X2> -> <X0> IS{r} <X2>.", r = r);
+        let f2 = format!("<X0> IS{start} <X1>; <X1> IS{start} <X2> -> <X0> IS{start} <X2>.", start = start);
         kb.tell( unsafe { mem::transmute( f2.as_str() ) });
-    }
-    let t1 = SystemTime::now();
-    for r in 0..opt.rules {
-        let f3 = format!("animal IS{r} thing.", r = r);
+        let f3 = format!("animal IS{start} thing.", start = start);
         kb.tell( unsafe { mem::transmute( f3.as_str() ) });
-        let f4 = format!("mammal IS{r} animal.", r = r);
+        let f4 = format!("mammal IS{start} animal.", start = start);
         kb.tell( unsafe { mem::transmute( f4.as_str() ) });
-        let f5 = format!("primate IS{r} mammal.", r = r);
+        let f5 = format!("primate IS{start} mammal.", start = start);
         kb.tell( unsafe { mem::transmute( f5.as_str() ) });
-        let f6 = format!("human IS{r} primate.", r = r);
+        let f6 = format!("human IS{start} primate.", start = start);
         kb.tell( unsafe { mem::transmute( f6.as_str() ) });
         for i in 0..opt.facts {
             let s = sets[(i % nsets) as usize];
-            let name = format!("{}{}{}", s, i, r);
-            let f = Box::leak(Box::new(format!("{name} ISA{r} {s}.", name = name, r = r, s = s)));
+            let name = format!("{}{}{}", s, i, start);
+            let f = Box::leak(Box::new(format!("{name} ISA{start} {s}.", name = name, start = start, s = s)));
             kb.tell( unsafe { mem::transmute( f.as_str() ) });
         }
-    }
-    let t2 = SystemTime::now();
+        if ((r % opt.report) == 0) || (r + 1 == opt.rules) {
+            for _s in 0..opt.samples {
+                thread::sleep(o_one_sec);
 
-    let t_r_micros = t1.duration_since(t0).unwrap().as_micros() as f64;
-    let t_f_micros = t2.duration_since(t1).unwrap().as_micros() as f64;
-    let t_r = t_r_micros / 1000000.0;
-    let t_f = t_f_micros / 1000000.0;
-    let num_r = (opt.rules * 2) as f64;
-    let t_r_1 = t_r_micros / (1000.0 * num_r);    
-    let num_f = (opt.rules * (opt.facts + 4)) as f64;
-    let t_f_1 = t_f_micros / (1000.0 * num_f);    
-    println!("{} {} {} {} {} {}", num_r, t_r, t_r_1, num_f, t_f, t_f_1);
+                let mut t_rs = vec![];
+                let mut t_fs = vec![];
+
+                for _x in 0..20 {
+                    start += 1;
+
+                    let t0 = SystemTime::now();
+
+                    let f1 = format!("<X0> ISA{start} <X1>; <X1> IS{start} <X2> -> <X0> ISA{start} <X2>.", start = start);
+                    kb.tell( unsafe { mem::transmute( f1.as_str() ) });
+                    let f2 = format!("<X0> IS{start} <X1>; <X1> IS{start} <X2> -> <X0> IS{start} <X2>.", start = start);
+                    kb.tell( unsafe { mem::transmute( f2.as_str() ) });
+
+                    let t1 = SystemTime::now();
+
+                    let f3 = format!("animal IS{start} thing.", start = start);
+                    kb.tell( unsafe { mem::transmute( f3.as_str() ) });
+                    let f4 = format!("mammal IS{start} animal.", start = start);
+                    kb.tell( unsafe { mem::transmute( f4.as_str() ) });
+                    let f5 = format!("primate IS{start} mammal.", start = start);
+                    kb.tell( unsafe { mem::transmute( f5.as_str() ) });
+                    let f6 = format!("human IS{start} primate.", start = start);
+                    kb.tell( unsafe { mem::transmute( f6.as_str() ) });
+                    for i in 0..opt.facts {
+                        let s = sets[(i % nsets) as usize];
+                        let name = format!("{}{}{}", s, i, start);
+                        let f = Box::leak(Box::new(format!("{name} ISA{start} {s}.", name = name, start = start, s = s)));
+                        kb.tell( unsafe { mem::transmute( f.as_str() ) });
+                    }
+                    let t2 = SystemTime::now();
+
+                    t_rs.push( t1.duration_since(t0).unwrap().as_micros() as f64 );
+                    t_fs.push( t2.duration_since(t1).unwrap().as_micros() as f64 );
+                }
+
+                
+                let t_r_micros: f64 = t_rs.iter().sum::<f64>() / t_rs.len() as f64;
+                let t_f_micros: f64 = t_fs.iter().sum::<f64>() / t_fs.len() as f64;
+                
+                let t_r_1 = t_r_micros / (1000.0 * num_r as f64);    
+                let t_f_1 = t_f_micros / (1000.0 * num_f as f64);    
+                println!("{} {} {} {}", num_r * start, t_r_1, num_f * start, t_f_1);
+            }
+        }
+    }
+    let t1 = SystemTime::now();
+    let total_time = t1.duration_since(t0).unwrap().as_micros() as f64 / 1000000.0;
+
+    println!("total time: {}", total_time);
+
 }
---
+++ python/benchmark_clips.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 by Enrique Pérez Arnaud <enrique@cazalla.net>
+#
+# This file is part of the whatever project.
+# https://whatever.cazalla.net
+#
+# The whatever project is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The whatever project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with any part of the terms project.
+# If not, see <http://www.gnu.org/licenses/>.
+import argparse
+import time
+import clips
+
+
+sets = ('thing', 'animal', 'mammal', 'primate', 'human')
+
+parser = argparse.ArgumentParser(description='Benchmark on ont.')
+parser.add_argument('-n', dest='n', type=int, default=2,
+                    help='number of sentences to add')
+parser.add_argument('-r', dest='r', type=int, default=1,
+                    help='batch of rules to run')
+parser.add_argument('-i', dest='i', type=int, default=4,
+                    help='number of samples per batch')
+parser.add_argument('-s', dest='s', type=int, default=1000,
+                    help='take a batch of samples every s rules')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    t = time.time()
+    start = 0
+    lsets = len(sets)
+    num_r = 2
+    num_f = 4 + args.n
+
+    def print_batch(start):
+        for n in range(args.i):
+            time.sleep(.5)
+            t_rs = []
+            t_fs = []
+            for x in range(20):
+                start += 1
+                t_0 = time.time()
+                clips.BuildRule("one%s" % start, "(is-a%s ?x1 ?x2) (a-is-a{r} ?x2 ?x3)" % start, "(assert (is-a%s ?x1 ?x3))" % start, "belogns%s" % start)
+                clips.BuildRule("two%s" % start, "(a-is-a%s ?x1 ?x2) (a-is-a{r} ?x2 ?x3)" % start, "(assert (a-is-a%s ?x1 ?x3))" % start, "subset%s" % start)
+                t_1 = time.time()
+                clips.Assert("(a-is-a%s animal thing)" % start)
+                clips.Assert("(a-is-a%s mammal animal)" % start)
+                clips.Assert("(a-is-a%s primate mammal)" % start)
+                clips.Assert("(a-is-a%s human primate)" % start)
+                for i in range(args.n):
+                    s = sets[i % lsets]
+                    name = '%s%d%d' % (s, i, r)
+                    clips.Assert("(is-a%s %s %s)" % (start, name, s))
+                clips.Run()
+                t_2 = time.time()
+                t_rs.append(t_1 - t_0)
+                t_fs.append(t_2 - t_1)
+
+            t_r = sum(t_rs) / len(t_rs)
+            t_f = sum(t_fs) / len(t_fs)
+            t_r_1 = t_r * 1000 / num_r
+            t_f_1 = t_f * 1000 / num_f
+            print('%d %f %d %f' % (num_r * start, t_r_1, num_f * start, t_f_1))
+
+        return start
+
+    for r in range(args.r):
+        start += 1
+        clips.BuildRule("one%s" % start, "(is-a%s ?x1 ?x2) (a-is-a{r} ?x2 ?x3)" % start, "(assert (is-a%s ?x1 ?x3))" % start, "belogns%s" % start)
+        clips.BuildRule("two%s" % start, "(a-is-a%s ?x1 ?x2) (a-is-a{r} ?x2 ?x3)" % start, "(assert (a-is-a%s ?x1 ?x3))" % start, "subset%s" % start)
+        clips.Assert("(a-is-a%s animal thing)" % start)
+        clips.Assert("(a-is-a%s mammal animal)" % start)
+        clips.Assert("(a-is-a%s primate mammal)" % start)
+        clips.Assert("(a-is-a%s human primate)" % start)
+        for i in range(args.n):
+            s = sets[i % lsets]
+            name = '%s%d%d' % (s, i, r)
+            clips.Assert("(is-a%s %s %s)" % (start, name, s))
+
+        if r % args.s == 0:
+            clips.Run()
+            start = print_batch(start)
+
+    print_batch(start)
+
+    print("total min: ", (time.time() - t) / 60)
