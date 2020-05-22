102aa273f028f94292a26505ae37485a12f7f2ab
enriquepablo <enrique@cazalla.net>
2020-05-23 00:33:15 +0200

    getting there with transforms

--- src/transform.rs
+++ src/transform.rs
@@ -1,12 +1,6 @@
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
-use self::AstNode::*;
-use pest::error::Error;
 use pest::Parser;
-use std::ffi::CString;
 
+use crate::lexicon::Lexicon;
 use crate::matching::MPMatching;
 use crate::segment::MPSegment;
 
@@ -15,143 +9,86 @@ use crate::segment::MPSegment;
 pub struct TParser;
 
 
-pub fn parse(source: &str, matching: MPMatching) -> MPMatching {
-    let mut  = vec![];
+pub fn process_transforms(source: &str, mut matching: MPMatching, lexicon: &Lexicon) -> MPMatching {
+    let mut var: &MPSegment;
+    let mut val: &MPSegment;
 
-    let pairs = TParser::parse(Rule::transform, source).ok().unwrap().next().unwrap();
+    let pairs = TParser::parse(Rule::transforms, source).ok().unwrap();
     for pair in pairs {
         match pair.as_rule() {
+            Rule::var => {
+                var = lexicon.intern("var", pair.as_str(), true);
+            }
             Rule::expr => {
-                ast.push(Print(Box::new(build_ast_from_expr(pair))));
+                let (new_val, new_matching) = compile_expr(pair, matching, lexicon);
+                val = lexicon.intern("decimal", new_val.as_str(), true);
+                matching = new_matching;
             }
             _ => {}
         }
+        matching.insert(var, val);
     }
 
-    Ok(ast)
+    matching
 }
 
-fn build_ast_from_expr(pair: pest::iterators::Pair<Rule>) -> AstNode {
+fn compile_expr(pair: pest::iterators::Pair<Rule>, matching: MPMatching, lexicon: &Lexicon) -> (f64, MPMatching) {
     match pair.as_rule() {
-        Rule::expr => build_ast_from_expr(pair.into_inner().next().unwrap()),
+        Rule::expr => {
+            compile_expr(pair.into_inner().next().unwrap(), matching, lexicon)
+        },
         Rule::monadicExpr => {
             let mut pair = pair.into_inner();
-            let verb = pair.next().unwrap();
-            let expr = pair.next().unwrap();
-            let expr = build_ast_from_expr(expr);
-            parse_monadic_verb(verb, expr)
-        }
+            let op = pair.next().unwrap();
+            let termpair = pair.next().unwrap();
+            let (term, new_matching) = compile_expr(termpair, matching, lexicon);
+            (parse_monadic_op(op, term), matching)
+        },
         Rule::dyadicExpr => {
             let mut pair = pair.into_inner();
             let lhspair = pair.next().unwrap();
-            let lhs = build_ast_from_expr(lhspair);
-            let verb = pair.next().unwrap();
+            let (lhs, new_matching) = compile_expr(lhspair, matching, lexicon);
+            let op = pair.next().unwrap();
             let rhspair = pair.next().unwrap();
-            let rhs = build_ast_from_expr(rhspair);
-            parse_dyadic_verb(verb, lhs, rhs)
-        }
-        Rule::terms => {
-            let terms: Vec<AstNode> = pair.into_inner().map(build_ast_from_term).collect();
-            // If there's just a single term, return it without
-            // wrapping it in a Terms node.
-            match terms.len() {
-                1 => terms.get(0).unwrap().clone(),
-                _ => Terms(terms),
-            }
-        }
-        Rule::assgmtExpr => {
-            let mut pair = pair.into_inner();
-            let ident = pair.next().unwrap();
-            let expr = pair.next().unwrap();
-            let expr = build_ast_from_expr(expr);
-            AstNode::IsGlobal {
-                ident: String::from(ident.as_str()),
-                expr: Box::new(expr),
-            }
-        }
-        Rule::string => {
-            let str = &pair.as_str();
-            // Strip leading and ending quotes.
-            let str = &str[1..str.len() - 1];
-            // Escaped string quotes become single quotes here.
-            let str = str.replace("''", "'");
-            AstNode::Str(CString::new(&str[..]).unwrap())
-        }
+            let (rhs, matching) = compile_expr(rhspair, matching, lexicon);
+            (parse_dyadic_op(op, lhs, rhs), matching)
+        },
+        Rule::decimal => {
+            (pair.as_str() as f64, matching)
+        },
+        Rule::var => {
+            let var = lexicon.intern("var", pair.as_str(), true);
+            (matching.get(var).unwrap().text as f64, matching)
+        },
         unknown_expr => panic!("Unexpected expression: {:?}", unknown_expr),
     }
 }
 
-fn parse_dyadic_verb(pair: pest::iterators::Pair<Rule>, lhs: AstNode, rhs: AstNode) -> AstNode {
-    AstNode::DyadicOp {
-        lhs: Box::new(lhs),
-        rhs: Box::new(rhs),
-        verb: match pair.as_str() {
-            "+" => DyadicVerb::Plus,
-            "*" => DyadicVerb::Times,
-            "-" => DyadicVerb::Minus,
-            "<" => DyadicVerb::LessThan,
-            "=" => DyadicVerb::Equal,
-            ">" => DyadicVerb::LargerThan,
-            "%" => DyadicVerb::Divide,
-            "^" => DyadicVerb::Power,
-            "|" => DyadicVerb::Residue,
-            "#" => DyadicVerb::Copy,
-            ">." => DyadicVerb::LargerOf,
-            ">:" => DyadicVerb::LargerOrEqual,
-            "$" => DyadicVerb::Shape,
-            _ => panic!("Unexpected dyadic verb: {}", pair.as_str()),
-        },
+fn parse_dyadic_op(op: pest::iterators::Pair<Rule>, lhs: f64, rhs: f64) -> f64 {
+    match op.as_str() {
+        "-" => lhs - rhs,
+        "+" => lhs + rhs,
+        "**" => lhs.powf(rhs),
+        "*" => lhs * rhs,
+        "/" => lhs / rhs,
+        "%" => lhs % rhs,
+        _ => panic!("Unexpected dyadic operator: {}", op.as_str()),
     }
 }
 
-fn parse_monadic_verb(pair: pest::iterators::Pair<Rule>, expr: AstNode) -> AstNode {
-    AstNode::MonadicOp {
-        verb: match pair.as_str() {
-            ">:" => MonadicVerb::Increment,
-            "*:" => MonadicVerb::Square,
-            "-" => MonadicVerb::Negate,
-            "%" => MonadicVerb::Reciprocal,
-            "#" => MonadicVerb::Tally,
-            ">." => MonadicVerb::Ceiling,
-            "$" => MonadicVerb::ShapeOf,
-            _ => panic!("Unsupported monadic verb: {}", pair.as_str()),
-        },
-        expr: Box::new(expr),
+fn parse_monadic_op(op: pest::iterators::Pair<Rule>, term: f64) -> f64 {
+    match op.as_str() {
+        "-" => - term,
+        "log" => term.log2(),
+        "exp" => - term.exp(),
+        "sin" => - term.sin(),
+        "cos" => - term.cos(),
+        "tan" => - term.tan(),
+        "floor" => - term.floor(),
+        "ceil" => - term.ceil(),
+        "asin" => - term.asin(),
+        "acos" => - term.acos(),
+        "atan" => - term.atan(),
+        _ => panic!("Unexpected dyadic verb: {}", op.as_str()),
     }
-}
-
-fn build_ast_from_term(pair: pest::iterators::Pair<Rule>) -> AstNode {
-    match pair.as_rule() {
-        Rule::integer => {
-            let istr = pair.as_str();
-            let (sign, istr) = match &istr[..1] {
-                "_" => (-1, &istr[1..]),
-                _ => (1, &istr[..]),
-            };
-            let integer: i32 = istr.parse().unwrap();
-            AstNode::Integer(sign * integer)
-        }
-        Rule::decimal => {
-            let dstr = pair.as_str();
-            let (sign, dstr) = match &dstr[..1] {
-                "_" => (-1.0, &dstr[1..]),
-                _ => (1.0, &dstr[..]),
-            };
-            let mut flt: f64 = dstr.parse().unwrap();
-            if flt != 0.0 {
-                // Avoid negative zeroes; only multiply sign by nonzeroes.
-                flt *= sign;
-            }
-            AstNode::DoublePrecisionFloat(flt)
-        }
-        Rule::expr => build_ast_from_expr(pair),
-        Rule::ident => AstNode::Ident(String::from(pair.as_str())),
-        unknown_term => panic!("Unexpected term: {:?}", unknown_term),
-    }
-}
-
-fn main() {
-    let unparsed_file = std::fs::read_to_string("example.ijs").expect("cannot read ijs file");
-    let astnode = parse(&unparsed_file).expect("unsuccessful parse");
-    println!("{:?}", &astnode);
 }
